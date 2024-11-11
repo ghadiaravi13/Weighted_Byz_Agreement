2cc05bab305e0015c4ec9d8223934822f3f8203f
Ravi Ghadia <ghadiaravi13@gmail.com>
2024-11-10 23:10:05 -0600

    cleanup

--- queen.py
+++ queen.py
@@ -2,54 +2,6 @@ import numpy as np
 import pandas as pd
 import config
 import copy
-# trusted_procs = []
-# untrusted_procs = []
-
-# total_procs = total_procs
-
-# trusted_proc_frac = 0.1
-# untrusted_proc_frac = 1 - trusted_proc_frac
-
-# trusted_proc_count = total_procs * trusted_proc_frac
-# untrusted_proc_count = total_procs * untrusted_proc_frac
-
-# proc_idx = np.arange(total_procs)
-
-# trusted_proc_failure_rate = 0.1
-# untrusted_proc_failure_rate = 0.5
-
-# fault_flag = np.array([np.random.choice(['F','C'],p=[untrusted_proc_failure_rate,1-untrusted_proc_failure_rate]) if i < untrusted_proc_count else np.random.choice(['F','C'],p=[trusted_proc_failure_rate,1-trusted_proc_failure_rate]) for i in proc_idx])
-# print(np.unique(fault_flag[:int(untrusted_proc_count)],return_counts=True),np.unique(fault_flag[int(untrusted_proc_count):],return_counts=True))
-# print("\n")
-
-# #RHO < 1/4 for Queen's
-# faulty_weight = 0.20
-# correct_weight = 1 - faulty_weight
-# weights = np.random.randint(low=500,size=total_procs)
-# weights = weights.astype(np.float64)
-# weights[fault_flag=='F'] *= faulty_weight/weights[fault_flag=='F'].sum()
-# weights[fault_flag=='C'] *= correct_weight/weights[fault_flag=='C'].sum()
-# # print(pd.Series(weights[fault_flag=='F']).describe())
-
-# # shuffle the process IDs
-# np.random.shuffle(proc_idx)
-
-# fault_flag = fault_flag[proc_idx]
-# weights = weights[proc_idx]
-# # print(weights[fault_flag=='C'].sum())
-
-# # alpha_rho
-# k = 1
-# while(1):
-#     top_k_indices = np.argpartition(weights, -k)[-k:]
-#     if(weights[top_k_indices].sum()>faulty_weight):
-#         print(f"\n--------------------{k} Queens:\n\n",top_k_indices,"\n---------------------\n")
-#         alpha_rho = k
-#         # queens = top_k_indices
-#         break
-#     else: k+=1
-
-# init vector matrix
 
 def weighted_byzantine_queen(total_procs, faulty_weight, user_overwrite=None, proposed_values=None):
     # Check if weights_file is provided and load weights
